4deea425ede2d71b465bada4ed84617a16a91fad
Mohd Afroj <mohd.afroj@gmail.com>
2025-02-04 14:40:37 +0530

    Add project 02 remaining files

--- project-02/src/App.tsx
+++ project-02/src/App.tsx
@@ -1,7 +1,28 @@
+import React from "react";
 import "./App.css";
+import Header from "./components/Header";
+//import Example from "./components/Example";
 
 function App() {
-  return <>Hello</>;
+  return (
+    <React.Fragment>
+      <Header />
+      {/* <Example /> */}
+      <div className="container">
+        <h2>Contact Us</h2>
+        <p>
+          LET’S CONNECT: WE’RE HERE TO HELP, AND WE’D LOVE TO HEAR FROM YOU!
+          WHETHER YOU HAVE A QUESTION, COMMENT, OR JUST WANT TO CHAT , YOU CAN
+          REACH OUT TO US THROUGH THE CONTACT FORM OF THIS PAGE, OR BY PHONE,
+          EMAIL, OR SOCIAL MEDIA.{" "}
+        </p>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default App;
