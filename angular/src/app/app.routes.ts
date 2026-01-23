067cd09d727ddc1be12ecf27396604af4786e037
Mohd Afroj <mohd.afroj@rajyasabha.digital>
2026-01-23 18:07:02 +0530

    test

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,9 +1,21 @@
 import { Routes } from '@angular/router';
 import { Websocket } from './pages/websocket/websocket';
+import { Home } from './pages/home/home';
+import { MainLayout } from './layout/main-layout/main-layout';
 
 export const routes: Routes = [
     {
-        path: 'websocket',
-        component: Websocket,
+        path: '',
+        component: MainLayout,
+        children: [
+            {
+                path: '',
+                component: Home
+            },
+            {
+                path: 'websocket',
+                component: Websocket,
+            }
+        ]
     }
 ];
